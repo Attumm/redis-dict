4435fd294a4bf6848c35734f989c88fe412951dd
Attumm <bijman.m.m@gmail.com>
2021-02-11 16:09:21 +0100

    v1.4

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
 
-    version='1.2',
+    version='1.4',
     py_modules=['redis_dict'],
     install_requires=['redis', 'future'],
     license='MIT',
