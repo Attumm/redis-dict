b65a5a175e6c7b9141c98e3661111d725075cd00
Attumm <Bijman.M.M@gmail.com>
2024-10-29 15:17:59 +0100

    changed version (#65)

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
 
-    version='2.6.0',
+    version='2.7.0',
     py_modules=['redis_dict'],
     install_requires=['redis',],
     license='MIT',
