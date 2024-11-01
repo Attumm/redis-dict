c61552b815040100462d4026cf9721be145e1590
Attumm <bijman.m.m@gmail.com>
2024-11-01 01:41:53 +0100

    Changed working version of build docs, but still needs work

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 set -e
 
-rm -rf docs/build/*
-rm -rf docs/source/*
+rm -rf docs/Makefile docs/build/* docs/source/*
 
-#python3 -m venv docs_venv
+python3 -m venv docs_venv
 
 source dev_venv/bin/activate
-#pip install --upgrade pip
-#pip install -e ".[docs]"
-
-#echo "Checking docstring style..."
-
-#echo "Checking docstring arguments..."
+pip install --upgrade pip
+pip install -e ".[docs]"
 
 python3 scripts/generate_sphinx_config.py
 
