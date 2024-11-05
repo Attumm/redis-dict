aeab4886ae69abbf5cac1783ecdb406811b624d0
Attumm <Bijman.M.M@gmail.com>
2024-11-05 13:13:11 +0100

    Add deploy action (#74)

--- scripts/build_dev_checks.sh
+++ scripts/build_dev_checks.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -e
 
-rm -rf dev_venv
+rm -rf .venv_dev
 python3 -m venv .venv_dev
 source .venv_dev/bin/activate
 
 pip install --upgrade pip
 pip install -e ".[dev]"
-
-pip freeze > dev-requirements.txt
 
 # Type Check
 python -m mypy
